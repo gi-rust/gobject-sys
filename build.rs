463db2528018ec7a92c41ef9b52009d3b5afc220
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2015-10-24 00:17:44 +0300

    Switch to GLib 2.46.x
    This inaugurates the stable branch glib-2-46, which will follow the
    eponymous branch in the GLib repository.

--- build.rs
+++ build.rs
@@ -3,5 +3,5 @@ extern crate pkg_config;
 use pkg_config::Config;
 
 fn main() {
-    Config::new().atleast_version("2.47").find("gobject-2.0").unwrap();
+    Config::new().atleast_version("2.46").find("gobject-2.0").unwrap();
 }
